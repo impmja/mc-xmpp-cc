fb7680a82a4c6c8ba41ae3db866dd402afbee713
Jan Schulte <jschulte@stud-hs.bremen.de>
2013-04-16 17:05:13 +0200

    First commit.

---
+++ MC-XMPP-CC/MC-XMPP-CC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MC-XMPP-CC
+//
+//  Created by Jan Schulte on 16.04.13.
+//  Copyright (c) 2013 Jan Schulte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MC-XMPP-CC/MC-XMPP-CC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MC-XMPP-CC
+//
+//  Created by Jan Schulte on 16.04.13.
+//  Copyright (c) 2013 Jan Schulte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
