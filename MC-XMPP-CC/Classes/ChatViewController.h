f37b1afa635d708b9f5509c2b9b515b1a37d9c96
Jan Schulte <jschulte@stud-hs.bremen.de>
2013-05-24 15:04:29 +0200

    Chat View Controller um Text Feld erweitert.

--- MC-XMPP-CC/Classes/ChatViewController.h
+++ MC-XMPP-CC/Classes/ChatViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+
+// Chat Messages in History speichern/laden: http://stackoverflow.com/questions/8568910/storing-messages-using-xmppframework-for-ios
 @interface ChatViewController : UITableViewController
 
 @end
